ac295f969633c493b84b09b73513ba308b09612a
TatsianaMark <tanya030890@mail.ru>
2020-10-13 22:58:48 +0300

    feat: add files

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,1 +1,7 @@
 @@include('actions.js')
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
+        $('.header__burger,.header__menu,.header__body, .content, .header__logo,.burger__btn').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
--- Marvel_layout-main/js/script.js
+++ Marvel_layout-main/js/script.js
@@ -2,3 +2,9 @@ function openMenu() {
     document.getElementById("header__burger").classList.toggle('active');
     
 }
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
+        $('.header__burger,.header__menu,.header__body, .content, .header__logo,.burger__btn').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
